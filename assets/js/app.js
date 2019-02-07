d05025ed9b888d5ca547f2bd4055e63c15ab2514
Sulejman Ljimaj <sulejman-limaj@hotmail.be>
2019-02-07 11:08:05 +0000

    added template literals

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
   var fahrenheit, celsius;
-  const weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather";
-  var apiKey = "b25ca364bc04acd2ca3e3aa725eb7097"; //<weather-api-keys>
+  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather`;
+  var apiKey = `b25ca364bc04acd2ca3e3aa725eb7097`; //<weather-api-keys>
   getLatLong();
 
   function getLatLong() {
     $.ajax({
-      url: "https://geoip-db.com/json/",
-      type: 'GET',
-      dataType: 'json',
+      url: `https://geoip-db.com/json/`,
+      type: `GET`,
+      dataType: `json`,
       success: function(data) {
-        var lat = data.latitude; //access latitude 
-        var long = data.longitude; //access longitude
-        let weatherUrl = weatherApiUrl + "?lat=" + lat + "&lon=" + long + "&APPID=" + apiKey + "&units=metric"; //Change weather 
+        var lat = data.latitude;
+        var long = data.longitude;
+        let weatherUrl = `${weatherApiUrl}?lat=${lat}&lon=${long}&APPID=${apiKey}&units=metric`; //Change weather 
         //in local scope
         getWeatherData(weatherUrl, data.city); //get data to show in browser. Access city as argument?
       },
       error: function(err) {
-        alert('Oops something went wrong, Please try again.');
+        alert(`Oops something went wrong, Please try again.`);
         console.log(err);
       }
     });
   }
-  $("#searchCity").submit(function(e) {
+  $(`#searchCity`).submit(function(e) {
     e.preventDefault();
-    let cityName = $("#city-name").val();
+    let cityName = $(`#city-name`).val();
     getWeather(cityName);
   });
 
   function getWeather(cityName) {
     console.log(cityName);
-    weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${apiKey}&units=metric`;
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${apiKey}&units=metric`;
     getWeatherData(weatherUrl, cityName); //is cityName assgined to the city parameter?
   }
 
   function getWeatherData(weatherUrl, city) { //data function
     $.ajax({
-      url: weatherUrl, //Not sure where am getting this info from, is the content of this parameter assigned in the getWeather //function?
-      type: 'GET',
-      dataType: 'json',
+      url: weatherUrl,
+      type: `GET`,
+      dataType: `json`,
       success: function(data) {
         var temprature = Math.round(data.main.temp);
         celsius = temprature;
@@ -49,34 +49,34 @@ $(document).ready(function() {
         var pressure = data.main.pressure;
         var sunrise = data.sys.sunrise;
         var sunset = data.sys.sunset;
-        $('.city').html(city);
-        $('.weatherDetail').html(weatherDetail); //update weather description in html
-        $('.iconpic>img').attr('src', 'http://openweathermap.org/img/w/' + icon + '.png'); //update the icon based on weather
-        $('.temp').html('temperature' + '<br>' + temprature + "&#8451;"); //update the temprature
-        $('.humidity').html('Humidity: ' + humidity + '%');
-        $('.pressure').html('Pressure: ' + pressure);
-        $('.sunrise').html('Sunrise: ' + sunrise);
-        $('.sunset').html('Sunset: ' + sunset);
+        $(`.city`).html(`City<br>${city}`);
+        $(`.weatherDetail`).html(weatherDetail);
+        $(`.iconpic>img`).attr(`src`, `http://openweathermap.org/img/w/${icon}.png`); //update the icon based on weather
+        $('.temp').html(`temperature<br>${temprature}&#8451;`); //update the temprature
+        $('.humidity').html(`Humidity: ${humidity}%`);
+        $('.pressure').html(`Pressure: ${pressure}`);
+        $('.sunrise').html(`Sunrise: ${sunrise}`);
+        $('.sunset').html(`Sunset: ${sunset}`);
       },
       error: function(err) {
-        alert('Oops something went wrong, Please try again.');
+        alert(`Please type a City name`);
         console.log(err);
       }
     });
   }
 
-  $('.toggle .button').click(function() {
+  $(`.toggle .button`).click(function() {
     // if the div has attribute id as c then convert temperature to fahrenheit
-    if ($('.toggle').attr('id') == 'c') {
-      $('.temp').html('temperature' + '<br>' + fahrenheit + "&#8457;");
-      $('.toggle').attr('id', 'f');
-      $(".button").html("Get Celsius");
+    if ($(`.toggle`).attr(`id`) == `c`) {
+      $(`.temp`).html(`temperature<br>${fahrenheit}&#8457;` );
+      $(`.toggle`).attr(`id`, `f`);
+      $(`.button`).html(`Get Celsius`);
     }
-    else if ($('.toggle').attr('id') == 'f') {
+    else if ($(`.toggle`).attr(`id`) == `f`) {
       //else if div has attribute id as f than convert temperature to celsius
-      $('.temp').html('temperature' + '<br>' + celsius + "&#8451;");
-      $('.toggle').attr('id', 'c');
-      $(".button").html("Get Fahrenheit");
+      $(`.temp`).html(`temperature<br>${celsius}&#8451;`);
+      $(`.toggle`).attr(`id`, `c`);
+      $(`.button`).html(`Get Fahrenheit`);
     }
   });
 });
