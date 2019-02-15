04db86c979c331b8de7ee53855d5f15d0b5cb60f
Sulejman Ljimaj <sulejman-limaj@hotmail.be>
2019-02-15 08:10:50 +0000

    minor change on testing

--- assets/js/appSpec.js
+++ assets/js/appSpec.js
@@ -1,5 +1,5 @@
 describe("formatTime function", function() {
-  it("Should return '7:54 GMT -1' when '1550213654' is passed in", () => {
+  it("Should return 6:50 + GMT offset from that timezone when '1550213654' is passed in", () => {
     expect(expect(formatTime(1550209817).substring(0, 8)).toBe("6:50 GMT"));
   });
 });
